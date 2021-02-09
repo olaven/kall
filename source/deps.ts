a16eee3afd09d97e3be37d8378d887944fd8a82d
Olav Sundfør <olav@sundfoer.com>
2021-02-09 12:49:41 +0100

    simplified test setup - huge tradeoff-> tests depend on external api

--- source/deps.ts
+++ source/deps.ts
@@ -1,7 +1,6 @@
-export { serve } from "https://deno.land/std@0.60.0/http/server.ts";
-export { encode } from "https://deno.land/std@v0.60.0/encoding/utf8.ts";
+export { serve } from "https://deno.land/std@0.86.0/http/server.ts";
+export { encode } from "https://deno.land/std@0.86.0/encoding/utf8.ts";
 export {
   assertEquals,
   assertNotEquals,
-  assertStringContains,
-} from "https://deno.land/std@v0.60.0/testing/asserts.ts";
+} from "https://deno.land/std@0.86.0/testing/asserts.ts";
