ba0b4e508e3cea55be57bd16e116d125b48176af
Tim Hårek Andreassen <tim@harek.no>
2024-04-05 16:52:59 +0200

    chore: Remove redundant `async`
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- source/methods.ts
+++ source/methods.ts
@@ -1,25 +1,25 @@
 import { performRequest } from "../source/support.ts";
 
-export const patch = async <T, G = T>(
+export const patch = <T, G = T>(
   url: string,
   payload: T,
   init: RequestInit = {},
 ) => performRequest<T, G>("PATCH", url, payload, init);
 
-export const del = async <T, G = T>(url: string, init: RequestInit = {}) =>
+export const del = <T, G = T>(url: string, init: RequestInit = {}) =>
   performRequest<T, G>("DELETE", url, null, init);
 
-export const put = async <T, G = T>(
+export const put = <T, G = T>(
   url: string,
   payload: T,
   init: RequestInit = {},
 ) => performRequest<T, G>("PUT", url, payload, init);
 
-export const post = async <T, G = T>(
+export const post = <T, G = T>(
   url: string,
   payload: T,
   init: RequestInit = {},
 ) => performRequest<T, G>("POST", url, payload, init);
 
-export const get = async <T, G = T>(url: string, init: RequestInit = {}) =>
+export const get = <T, G = T>(url: string, init: RequestInit = {}) =>
   performRequest<T, G>("GET", url, null, init);
