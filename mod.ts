f0612326c5c025499688a23ff5a1e5fd7f65de62
Tim Hårek Andreassen <tim@harek.no>
2024-04-05 16:34:50 +0200

    chore: Update stuff
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- mod.ts
+++ mod.ts
@@ -1,3 +1,2 @@
 export { del, get, patch, post, put } from "./source/methods.ts";
-export { filterBody, filterResponse, filterStatus } from "./source/filters.ts";
 export { STATUS_CODE } from "./source/codes.ts";
--- tests/methods.test.ts
+++ tests/methods.test.ts
@@ -1,28 +1,23 @@
 import { assertEquals, assertNotEquals } from "../source/deps.ts";
 import { get } from "../source/methods.ts";
 
-
-const url = "https://api.entur.io/"
+const url = "https://api.entur.io/";
 
 Deno.test(
   "Get returns something",
   async () => {
-
     const returned = await get(url);
 
     assertNotEquals(returned, null);
     assertNotEquals(returned, undefined);
     assertNotEquals(returned, []);
-  }
+  },
 );
 
 Deno.test(
   "Get returns correct status",
   async () => {
-
-    const [status, body] = await get(url);
+    const { status } = await get(url);
     assertEquals(status, 404);
   },
 );
-
-
