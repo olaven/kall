350c8f30621f1fe2431376bf6670685f84ce6d18
Tim Hårek Andreassen <tim@harek.no>
2024-04-05 16:18:58 +0200

    refactor: Make status codes into enum instead of single vars
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- tests/codes.test.ts
+++ tests/codes.test.ts
@@ -1,13 +1,12 @@
-import { IM_A_TEAPOT, LOOP_DETECTED, PRECONDITION_REQUIRED, RANGE_NOT_SATISFIABLE, SEE_OTHER, URI_TOO_LONG } from "../source/codes.ts";
+import { STATUS_CODE } from "../source/codes.ts";
 import { assertEquals } from "../source/deps.ts";
 const { test } = Deno;
 
 test("Some random codes", () => {
-
-    assertEquals(508, LOOP_DETECTED);
-    assertEquals(428, PRECONDITION_REQUIRED);
-    assertEquals(418, IM_A_TEAPOT);
-    assertEquals(416, RANGE_NOT_SATISFIABLE);
-    assertEquals(414, URI_TOO_LONG);
-    assertEquals(303, SEE_OTHER);
+  assertEquals(508, STATUS_CODE.LOOP_DETECTED);
+  assertEquals(428, STATUS_CODE.PRECONDITION_REQUIRED);
+  assertEquals(418, STATUS_CODE.IM_A_TEAPOT);
+  assertEquals(416, STATUS_CODE.RANGE_NOT_SATISFIABLE);
+  assertEquals(414, STATUS_CODE.URI_TOO_LONG);
+  assertEquals(303, STATUS_CODE.SEE_OTHER);
 });
