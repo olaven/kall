e79032bcbaca41c2996d7f330b8d8fbe10d1683a
Tim Hårek Andreassen <tim@harek.no>
2024-04-05 16:51:42 +0200

    chore: Formatting
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- source/methods.ts
+++ source/methods.ts
@@ -9,8 +9,11 @@ export const patch = async <T, G = T>(
 export const del = async <T, G = T>(url: string, init: RequestInit = {}) =>
   performRequest<T, G>("DELETE", url, null, init);
 
-export const put = async <T, G = T>(url: string, payload: T, init: RequestInit = {}) =>
-  performRequest<T, G>("PUT", url, payload, init);
+export const put = async <T, G = T>(
+  url: string,
+  payload: T,
+  init: RequestInit = {},
+) => performRequest<T, G>("PUT", url, payload, init);
 
 export const post = async <T, G = T>(
   url: string,
